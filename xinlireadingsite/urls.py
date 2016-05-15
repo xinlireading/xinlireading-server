2c448792af3567e0567caf112b018685912c1b31
Yang Cun <yangcun@live.com>
2016-05-15 20:24:02 +0800

    add the static urlpatterns.

--- xinlireadingsite/urls.py
+++ xinlireadingsite/urls.py
@@ -19,4 +19,4 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^', include('xinlireading.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
