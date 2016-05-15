3671bc0db4f44326efe06247eb4f97f691b11681
Yang Cun <yangcun@live.com>
2016-05-15 17:02:28 +0800

    change the url to root.

--- xinlireadingsite/urls.py
+++ xinlireadingsite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^xinlireading/', include('xinlireading.urls')),
+	url(r'^', include('xinlireading.urls')),
     url(r'^admin/', admin.site.urls),
 ]
